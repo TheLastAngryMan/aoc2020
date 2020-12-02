88d5b3f16db85cd1110c185726a8488195721785
TheLastAngryMan <unknown@somewhere.com>
2020-12-02 14:29:58 +0000

    Added day two solution

---
+++ src/main/scala/aoc2020/DayTwo.scala
@@ -0,0 +1,49 @@
+package aoc2020
+
+import scala.io.Source
+import scala.util.matching.Regex
+
+object DayTwo {
+  private val LOWER = "lower"
+  private val UPPER = "upper"
+  private val CHARACTER = "character"
+  private val PASSWORD = "password"
+  val passwordRegex = new Regex("^([0-9]*)-([0-9]*) ([a-z]): (.*)$", LOWER, UPPER, CHARACTER, PASSWORD)
+
+  case class PasswordLine(lowerLimit: Int, upperLimit: Int, character: Char, password: String) {
+    def validatePartOne: Boolean = {
+      val numbRepeats = password.toCharArray.count(_.toLower == character)
+      numbRepeats >= lowerLimit && numbRepeats <= upperLimit
+    }
+
+    def validatePartTwo: Boolean = {
+      val passwordCharacters = password.toList
+      passwordCharacters.lift(lowerLimit - 1).contains(character) ^ passwordCharacters.lift(upperLimit - 1).contains(character)
+    }
+  }
+
+  def parsePasswordLine(input: String): Option[PasswordLine] = {
+    passwordRegex.findAllIn(input).matchData.toList.headOption.flatMap { matches =>
+      if (matches.groupCount == 4) {
+        Some(
+          PasswordLine(
+            matches.group(LOWER).toInt,
+            matches.group(UPPER).toInt,
+            matches.group(CHARACTER).toCharArray.head,
+            matches.group(PASSWORD)
+          )
+        )
+      } else None
+    }
+  }
+
+  def validatePasswords(input: Seq[String], f: PasswordLine => Boolean): Int = input.flatMap(parsePasswordLine).count(f)
+
+  def main(args: Array[String]): Unit = {
+    val dayOneFile = Source.fromFile("inputs/dayTwo.txt")
+    val dayOneInput = dayOneFile.getLines.toList
+    println(s"Valid passwords part one: ${validatePasswords(dayOneInput, _.validatePartOne)}")
+    println(s"Valid passwords part two: ${validatePasswords(dayOneInput, _.validatePartTwo)}")
+    dayOneFile.close()
+  }
+}
