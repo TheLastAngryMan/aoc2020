dd1088542fd886b9f5f85850f2df463e67786b1d
TheLastAngryMan <somewhere@nowhere.com>
2020-12-08 18:00:48 +0000

    Reformatted all files

--- src/main/scala/aoc2020/Day5.scala
+++ src/main/scala/aoc2020/Day5.scala
@@ -4,18 +4,24 @@ import scala.annotation.tailrec
 
 object Day5 extends App {
   @tailrec
-  def processBinarySpace(lines: List[Char], lowerBound: Int, upperBound: Int, lowerHalfCharacter:Char, upperHalfCharacter: Char): Int = {
+  def processBinarySpace(
+      lines: List[Char],
+      lowerBound: Int,
+      upperBound: Int,
+      lowerHalfCharacter: Char,
+      upperHalfCharacter: Char
+  ): Int = {
     lines match {
       case Nil => lowerBound
       case head :: tail =>
         val boundDifference = ((upperBound - lowerBound) / 2) + 1
         val newLower = if (head == upperHalfCharacter) lowerBound + boundDifference else lowerBound
-        val newUpper = if (head == lowerHalfCharacter)  upperBound - boundDifference else upperBound
+        val newUpper = if (head == lowerHalfCharacter) upperBound - boundDifference else upperBound
         processBinarySpace(tail, newLower, newUpper, lowerHalfCharacter, upperHalfCharacter)
     }
   }
   def processRow(lines: List[Char]): Int = processBinarySpace(lines, 0, 127, 'F', 'B')
-  def processSeat(lines: List[Char]): Int = processBinarySpace(lines, 0,7, 'L', 'R')
+  def processSeat(lines: List[Char]): Int = processBinarySpace(lines, 0, 7, 'L', 'R')
   def processBoardingPass(lines: List[Char]): Int = processRow(lines.slice(0, 7)) * 8 + processSeat(lines.slice(7, 10))
 
   Util.processInput("day5") { lines =>
@@ -27,7 +33,7 @@ object Day5 extends App {
     val sortedIds = boardingPassIds.sorted
     val (nextSeatId, _) = sortedIds.zipWithIndex.filter {
       case (value, index) if index > 0 => sortedIds(index - 1) != value - 1
-      case _ => false
+      case _                           => false
     }.head
     println(s"Boarding pass id is ${nextSeatId - 1}")
   }
--- src/main/scala/aoc2020/Day6.scala
+++ src/main/scala/aoc2020/Day6.scala
@@ -4,23 +4,29 @@ import scala.annotation.tailrec
 
 object Day6 extends App {
   @tailrec
-  def processDeclarations(lines: List[String], currentSet: Seq[Set[Char]] = Seq.empty, processedGroups: Seq[Seq[Set[Char]]] = Seq.empty): Seq[Seq[Set[Char]]] = {
+  def processDeclarations(
+      lines: List[String],
+      currentSet: Seq[Set[Char]] = Seq.empty,
+      processedGroups: Seq[Seq[Set[Char]]] = Seq.empty
+  ): Seq[Seq[Set[Char]]] = {
     lines match {
-      case Nil => processedGroups :+ currentSet
+      case Nil                          => processedGroups :+ currentSet
       case head :: tail if head.isEmpty => processDeclarations(tail, Seq.empty, processedGroups :+ currentSet)
-      case head :: tail => processDeclarations(tail, currentSet :+ head.toList.toSet, processedGroups)
+      case head :: tail                 => processDeclarations(tail, currentSet :+ head.toList.toSet, processedGroups)
     }
   }
 
   def sumOfAnsweredQuestions(lines: List[String]): Int = {
-    processDeclarations(lines).map(_.foldLeft(Set.empty[Char])((a,b) => a ++ b).size).sum
+    processDeclarations(lines).map(_.foldLeft(Set.empty[Char])((a, b) => a ++ b).size).sum
   }
 
   def sumOfAllAnsweredQuestions(lines: List[String]): Int = {
-    processDeclarations(lines).map(answers => answers.foldLeft(answers.headOption.getOrElse(Set.empty))((a, b) => a.intersect(b)).size).sum
+    processDeclarations(lines)
+      .map(answers => answers.foldLeft(answers.headOption.getOrElse(Set.empty))((a, b) => a.intersect(b)).size)
+      .sum
   }
 
-  Util.processInput("day6") {lines =>
+  Util.processInput("day6") { lines =>
     println(s"Total number of answered questions: ${sumOfAnsweredQuestions(lines)}")
     println(s"Total number of all answered questions: ${sumOfAllAnsweredQuestions(lines)}")
   }
